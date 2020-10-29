feb6bf62dc204befb678755afef2b7c500e45a93
unknown <mitesharora96@gmail.com>
2020-10-29 18:33:40 +0530

    Implemented sales details table and header comp

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,25 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import {HttpClientModule} from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SalesTableComponent } from './sales-table/sales-table.component';
+import { HeaderComponent } from './header/header.component';
+
+import {TableModule} from 'primeng/table';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SalesTableComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    HttpClientModule,
+    TableModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/sales-table/sales-table.component.ts
@@ -0,0 +1,23 @@
+import { Component, OnInit } from '@angular/core';
+import {SalesDetailService} from '../sales-detail.service'
+
+@Component({
+  selector: 'app-sales-table',
+  templateUrl: './sales-table.component.html',
+  styleUrls: ['./sales-table.component.css']
+})
+export class SalesTableComponent implements OnInit {
+
+  details:any[];
+  constructor(private sDetails:SalesDetailService) { }
+
+  ngOnInit(): void {
+    this.sDetails.getSalesDetails().subscribe(
+      (data)=>{
+        this.details=data;
+      }
+    )
+
+  }
+
+}
